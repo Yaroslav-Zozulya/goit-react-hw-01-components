214a28265c7d609021fc630c4cba71f1c81f62bd
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-05-26 17:49:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
         color: '#010101',
       }}
     >
-      <Pprofile
+      <Pasdrofile
         username={user.username}
         tag={user.tag}
         location={user.location}
